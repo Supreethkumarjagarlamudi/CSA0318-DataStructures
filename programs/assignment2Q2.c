6966c79e7cb96eb0328bdb8434268566c2f16224
Supreeth kumar jagarlamudi <supreethkumarjagarlamudi@Supreeths-MacBook-Pro.local>
2023-12-27 10:10:35 +0530

    changed assignmentQ

--- programs/assignment2Q2.c
+++ programs/assignment2Q2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 3, 1, 6, 5, 8};
+    int arr[] = {1, 2, 3, 4, 5, 3, 1, 6, 5, 8, 9, 10, 4, 5, 8};
     int size = sizeof(arr)/sizeof(arr[0]);
 
     for(int i = 0; i < size; i++){
